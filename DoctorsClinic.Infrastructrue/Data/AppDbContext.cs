62bfae82050f68032097350e5176a2629fcf4347
pilotmuhammed <mohammeditsec@gmail.com>
2025-08-06 10:29:27 +0300

    Update AppDbContext

--- DoctorsClinic.Infrastructrue/Data/AppDbContext.cs
+++ DoctorsClinic.Infrastructrue/Data/AppDbContext.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.EntityFrameworkCore;
-using DoctorsClinic.Domain.Entities;
+﻿using DoctorsClinic.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace DoctorsClinic.Infrastructure.Data
 {
+    // This class is used by EF Core Only when performing Add-Migration || Update-Database
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseNpgsql("Host=localhost;Database=DoctorsClinicDb;Username=postgres;Password=pilot");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
     public class AppDbContext : DbContext
     {
-
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+        
         public DbSet<Patient> Patients { get; set; }
         public DbSet<Doctor> Doctors { get; set; }
         public DbSet<User> Users { get; set; }
@@ -18,10 +34,6 @@ namespace DoctorsClinic.Infrastructure.Data
         public DbSet<Invoice> Invoices { get; set; }
         public DbSet<Payment> Payments { get; set; }
 
-        public AppDbContext(DbContextOptions<AppDbContext> options)
-            : base(options)
-        {
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
